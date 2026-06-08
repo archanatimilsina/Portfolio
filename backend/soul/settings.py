73f24238d63cd1cf51cd27e16eedc27b7f25e1bf
archanatimilsina <archanatimilsina88@gmail.com>
2026-06-08 13:22:33 +0545

    fix INSTALLED_APPS

--- backend/soul/settings.py
+++ backend/soul/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 EXTERNAL_APPS =[
     'api'
@@ -136,3 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
